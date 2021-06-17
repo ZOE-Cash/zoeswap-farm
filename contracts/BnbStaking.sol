d23aa2c38f63f787b2683b3d5f4f1d3cd46c0c95
Mauro Henao <mauro.henaog@gmail.com>
2021-06-17 00:38:31 -0500

    .

--- contracts/BnbStaking.sol
+++ contracts/BnbStaking.sol
@@ -1,9 +1,9 @@
 pragma solidity 0.6.12;
 
-import '@pancakeswap/pancake-swap-lib/contracts/math/SafeMath.sol';
-import '@pancakeswap/pancake-swap-lib/contracts/token/BEP20/IBEP20.sol';
-import '@pancakeswap/pancake-swap-lib/contracts/token/BEP20/SafeBEP20.sol';
-import '@pancakeswap/pancake-swap-lib/contracts/access/Ownable.sol';
+import '@zoeswap/zoeswap-lib/contracts/math/SafeMath.sol';
+import '@zoeswap/zoeswap-lib/contracts/token/BEP20/IBEP20.sol';
+import '@zoeswap/zoeswap-lib/contracts/token/BEP20/SafeBEP20.sol';
+import '@zoeswap/zoeswap-lib/contracts/access/Ownable.sol';
 
 // import "@nomiclabs/buidler/console.sol";
 
